e29bf9dbee25f6c24b42ce9917d6ce157d8b0236
Mohammad Rafaquat <Rafaquat.mala0031@student.monash,edu>
2022-10-08 11:12:44 +0800

    Tooltip on legend

---
+++ w9_interactive_charts/bubble_chart_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "bubble_chart.vg.json";
+vegaEmbed("#bubble_chart", vg_1).then(function(result) {
+    // Access the Vega view instance(https: //vega.github.io/vega/docs/api/view/) as result.view
+
+
+}).catch(console.error);

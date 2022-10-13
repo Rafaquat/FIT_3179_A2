ed026d7a354b2361f51f9d8a0ced806bd30e2172
Mohammad Rafaquat <Rafaquat.mala0031@student.monash,edu>
2022-10-14 01:52:06 +0800

    Changed directories

--- w9_interactive_charts/bubble_chart_vis.js
+++
@@ -1,6 +0,0 @@
-var vg_1 = "bubble_chart.vg.json";
-vegaEmbed("#bubble_chart", vg_1).then(function(result) {
-    // Access the Vega view instance(https: //vega.github.io/vega/docs/api/view/) as result.view
-
-
-}).catch(console.error);
